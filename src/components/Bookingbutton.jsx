eb8c82f5dcdc40f684f1acfdcfe63d81f01798c2
jon-coates <coatastic@gmail.com>
2025-09-08 19:39:45 +1000

    Update BookingButton link to new Timely bookings URL

--- src/components/Bookingbutton.jsx
+++ src/components/Bookingbutton.jsx
@@ -4,7 +4,7 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function BookingButton() {
   return (
       <a
-        href="http://fadedaz.gettimely.com"
+        href="https://bookings.gettimely.com/fadedaz/book"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-8 py-4 bg-white/90 text-black border-black/50 border-2 text-2xl font-bold font-dmserif rounded-full shadow-md hover:bg-zinc-200 hover:text-zinc-900 hover:-translate-y-1 active:bg-zinc-400 active:text-black transition-all"
